cea6910ba2fffffbedfa5a76e01887fab5211663
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-02-19 17:08:32 -0300

    Servico http, observable, subscribe,api mocadajson

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -9,11 +9,10 @@ import { RestaurantsService } from './restaurants.service';
 export class RestaurantsComponent implements OnInit {
 
   restaurants: Restaurant[]
-  
+
   constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
-    this.restaurants = this.restaurantsService.restaurants()
+    this.restaurantsService.restaurants().subscribe( restaurants => this.restaurants = restaurants)
    }
-
 }
