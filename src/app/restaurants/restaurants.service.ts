d9ecb469673228cde892da146d3870ceeeb6bcb0
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-02-21 16:16:47 -0300

    Introduzindo Pipe

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,7 +6,6 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import {ErrorHandler} from '../app.error-handler'
-import { errorHandler } from "@angular/platform-browser/src/browser";
 
 
 @Injectable()
@@ -22,6 +21,12 @@ export class RestaurantsService {
 
     restaurantById(id: string): Observable<Restaurant>{
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
