872efb906ec001ed95f1f2efe86e53fb39cc70d6
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-03-30 14:23:45 -0300

    Implementado Menu Item individual

--- src/app/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { Observable } from 'rxjs/Observable';
+import {ActivatedRoute} from '@angular/router'
+import {MenuItem} from '../menu-item/menu-item.model'
+
 
 @Component({
   selector: 'mt-menu',
@@ -6,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  menu: Observable<MenuItem[]>
+
+  constructor( private restaurantService: RestaurantsService, private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.menu = this.restaurantService.menuOfRestaurant(this.router.parent.snapshot.params['id'])
+  }
+
+  addMenuItem(item: MenuItem){
+    console.log(item)
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,8 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import {ErrorHandler} from '../app.error-handler'
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
+
 
 
 @Injectable()
@@ -30,4 +32,11 @@ export class RestaurantsService {
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
 }
