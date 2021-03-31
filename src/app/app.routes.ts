7d32d1bbda33d9bacdcc6a880a13a837929f896d
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-03-31 16:17:18 -0300

    Criacao do componente de compra (order)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,25 @@
-import {Routes} from '@angular/router'
+import { Routes } from '@angular/router'
 import { AboutComponent } from './about/about.component'
 import { HomeComponent } from './home/home.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
+import { OrderComponent } from './order/order.component'
 
 
 
-export const ROUTES: Routes =[
-    {path:'', component: HomeComponent},
-    {path:'restaurants', component: RestaurantsComponent},
-    {path:'restaurants/:id', component: RestaurantDetailComponent,
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'restaurants', component: RestaurantsComponent },
+    {
+        path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
-            {path: '', redirectTo: 'menu', pathMatch: 'full'},
-            {path: 'menu', component: MenuComponent},
-            {path: 'reviews', component: ReviewsComponent}
-        ]},
-    {path:'about', component: AboutComponent}
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent }
+        ]
+    },
+    { path: 'order', component: OrderComponent },
+    { path: 'about', component: AboutComponent }
 ]
