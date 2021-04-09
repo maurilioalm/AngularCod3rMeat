3cf3e01620ef81b07c21d7325e2f89bcae4808bb
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-04-09 12:30:09 -0300

    Componente para representar as formas pagamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component'
 import { FormsModule } from '@angular/forms';
-import { InputComponent } from './shared/input/input.component'
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component'
 
 
 @NgModule({
@@ -38,7 +39,8 @@ import { InputComponent } from './shared/input/input.component'
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
