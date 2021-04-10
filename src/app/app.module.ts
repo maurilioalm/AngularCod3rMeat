11e5a62fb22f679541eb1b64b641027f46009970
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-04-09 21:18:45 -0300

    Implementando componente items de compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,8 +22,8 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
 import { OrderComponent } from './order/order.component'
 import { FormsModule } from '@angular/forms';
 import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component'
-
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component'
 
 @NgModule({
   declarations: [
@@ -40,7 +40,8 @@ import { RadioComponent } from './shared/radio/radio.component'
     ReviewsComponent,
     OrderComponent,
     InputComponent,
-    RadioComponent
+    RadioComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
